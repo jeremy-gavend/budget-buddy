f0c00bbb923af24b7b5fcc6277ed7c9df009b1bc
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-03-17 14:47:35 +0100

    feat: SQL commands now works properly (withdraw, deposit, history)

--- Class_Operations.py
+++ Class_Operations.py
@@ -1,33 +1,36 @@
 from datetime import datetime
+from Class_Accounts import Accounts
 
 class Operations:
-    def withdraw(self, cursor, user, amount, description):
+    def withdraw(cursor, mydb, user: Accounts, amount: float, description):
         """Called by the button "withdraw" """
         balance = user.get_balance(cursor)
-        date = datetime.date()
+        current_date = datetime.today()
  
-        cursor.execute(f"UPDATE accounts SET balance = {balance - amount} WHERE user_id = {user.user.id}")
-        cursor.execute(f"INSERT INTO transactions (user_id, amount, description, date, type) VALUES ({user.username}, {amount}, {description}, {date}, 'withdraw')")
+        cursor.execute(f"UPDATE accounts SET balance = {balance - amount} WHERE user_id = {user.user.id};")
+        cursor.execute(f"INSERT INTO transactions (user_id, amount, description, date, type) VALUES ({user.username}, {amount}, {description}, {current_date}, 'withdraw');")
 
+        mydb.commit()
         # ! Display this in a informative textbox
-        return f"{user.username}, you successfully withdrew {amount} from your account at {date}."
+        return f"{user.username}, you successfully withdrew {amount} from your account at {current_date}."
 
-    def deposit(self, cursor, user, amount, description):
+    def deposit(cursor, mydb, user: Accounts, amount: float, description: str, category: str):
         """ Called by the button "deposit" """
         
         balance = user.get_balance(cursor)
-        date = datetime.date()
+        current_date = datetime.today()
 
-        cursor.execute(f"UPDATE accounts SET balance = {balance + amount} WHERE user_id = {user.user_id}")
-        cursor.execute(f"INSERT INTO transactions (user_id, amount, description, date, type) VALUES ({user.username}, {amount}, {description}, {date}, 'withdraw')")
+        cursor.execute(f"UPDATE accounts SET balance = {balance + amount} WHERE user_id = {user.user_id};")
+        cursor.execute(f"INSERT INTO transactions (user_id, amount, description, date, type, category) VALUES ({user.user_id}, {amount}, '{description}', '{current_date}', 'deposit', '{category}');")
 
+        mydb.commit()
         # ! Display this in a informative textbox
-        return f"{user.username}, you successfully withdrew {amount} from your account at {date}."
+        return f"{user.username}, you successfully withdrew {amount} from your account at {current_date}."
 
-    def transfert(self):
+    def transfert():
         # something for the display here
         return 
 
-    def history(self, cursor, user):
-        cursor.execute(f"SELECT * FROM transactions WHERE user_id = {user.user_id}")
+    def history(cursor, user):
+        cursor.execute(f"SELECT * FROM transactions WHERE user_id = {user.user_id};")
         return cursor.fetchall()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mysql.connector, pygame
-from Class_Account import Accounts
+from Class_Accounts import Accounts
+from Class_Operations import Operations
 
 #----- Local connection stuffs
 mydb = mysql.connector.connect(
@@ -13,9 +14,15 @@ cursor = mydb.cursor()
 #-----
 
 # Some login page
+# some resister page that create an user
+
+# --- Test ---
+username = 'test'
+password = 'test' #will be a hash
+# --- ---- ---
 
 try:
-    cursor.execute("SELECT * FROM users WHERE username = 'username' AND password = 'password'")
+    cursor.execute(f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}';")
 except Exception:
     print("Error in login")
 
@@ -23,4 +30,11 @@ user_data = cursor.fetchall()
 
 # Put the id of the user here for the session (should be overwritten when new session is created)
 # This is for select requests
-user = Accounts(user_data[0], user_data[1])
+user = Accounts(user_data[0][0], user_data[0][1])
+
+# user.account_creation(cursor, mydb)
+# balance = user.get_balance(cursor)
+
+# Operations.deposit(cursor, mydb, user, 100, "Test_deposit", "None")
+historic = Operations.history(cursor, user)
+print(historic)
