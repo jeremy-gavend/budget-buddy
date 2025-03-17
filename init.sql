57c8717fab6f828fa04bd96d5dc1e0188027de2c
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-03-17 10:05:08 +0100

    feat: Create a file to initialize the sql server

---
+++ init.sql
@@ -0,0 +1,24 @@
+# Launch only once to create a local database!!
+CREATE DATABASE budget_buddy;
+USE budget_buddy;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL
+);
+
+# Alternative: Create a new table when a new user is created; reuse a table with foreign key associated with user
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
